ddd4393bbd4872e1fd4ee0c17de190febcf9d7c0
Elisabeth Ngo <elisab.ngo@gmail.com>
2022-03-02 16:58:11 +0100

    header 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 // assets/js/script.js
 
-window.addEventListener('load', () => {
+TweenMax.to(".home__primary", 2, {
+    width: "100%",
+    ease: Expo.easeInOut
+})
+gsap.from('.home__title', {opacity: 0, duration: 2, delay: 1.5, y: 100})
+gsap.from('.home__img img', {opacity: 0, duration: 2, delay:1.5, y: -100})
+
+/*HOME SECONDARY*/
+TweenMax.to(".home__secondary", 2.5, {
+    width: "100%",
+    ease: Expo.easeInOut
+})
+gsap.from('.home__scroll', {opacity: 0, duration: 3, x: -100})
+gsap.from('.home__year', {opacity: -1, duration: 1.5, delay: 1.5, x: 100})
+
+/*window.addEventListener('load', () => {
 
     // smooth scroll
     const links = document.querySelectorAll('.scroll_to');
@@ -11,3 +26,4 @@ window.addEventListener('load', () => {
         })
     })
 })
+*/
